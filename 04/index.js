8e1ff1c68b827dff01ea8ce61946e7ee859efbfc
Edfram-Guerra <edfram.guerra@gmail.com>
2022-11-01 07:40:34 -0300

    Exercicio resolvido

---
+++ 04/index.js
@@ -0,0 +1,20 @@
+// Validação de numeros pares
+// Dado o array numérico abaixo, faça um programa que consiga validar se todos os números são pares. 
+// Se todos os itens do array forem par, o programa deverá imprimir array válido, senão array inválido.
+
+// Dica: utilize o método every()
+
+const numeros = [0, 122, 4, 6, 7, 8, 44]
+
+
+const validacaoNumerosPares = (arrayNumeros) => {
+    const resultado = arrayNumeros.every((number) => {
+        return number % 2 == 0;
+    });
+    if (resultado) {
+        console.log("Array válido")
+    } else {
+        console.log("Array inválido")
+    }
+}
+validacaoNumerosPares(numeros)
