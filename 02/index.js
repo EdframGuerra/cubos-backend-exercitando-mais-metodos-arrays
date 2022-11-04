588ce36d0c90b6c1b9fab34c876987e27f63a460
Edfram-Guerra <edfram.guerra@gmail.com>
2022-11-04 17:52:22 -0300

    Exercicio resolvido

--- 02/index.js
+++ 02/index.js
@@ -1,12 +1,6 @@
-// Verificando limite de caracters
-// Dado um determinado array de palavras, faça um programa que verifica se existe ao menos uma palavra com mais de 5 caracteres. 
-// Caso exista, o programa deverá imprimir existe palavra inválida, senão, o programa deverá imprimir array validado.
-
 const palavras = ["livro", "caneta", "sol", "carro", "orelha"]
 
-
-
-const tamanhoPalavra = palavras.every((palavra) => {
+const tamanhoPalavra = palavras.some((palavra) => {
     return palavra.length > 5;
 });
 if (tamanhoPalavra) {
